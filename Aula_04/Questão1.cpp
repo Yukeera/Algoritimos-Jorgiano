8e35880c579fc998c84453169e27e6715d851858
Fernando Yuri <107281250+Yukeera@users.noreply.github.com>
2025-04-16 18:58:02 +0000

    Questões lista exercicio

---
+++ Aula_04/Questão1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+long long soma(long long a, long long b) {
+long long r = a + b;
+return r;
+}
+
+int main() {
+long long a, b;
+std::cin >> a >> b;
+long long s = soma(a, b); std::cout << s << std::endl; return 0;
+}
---
+++ Aula_04/Questão2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int l1,l2;
+    cin >> l1 >> l2;
+    if ((l1 < 1 || l1 > 1000000000) || (l2 < 1 || l2 > 1000000000)){
+        
+    }
+}
